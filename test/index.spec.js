450ce74098d3c1d7df07a53f1a12ce0c228bcc3a
lxjwlt <lxjwlt.c@gmail.com>
2017-11-03 22:39:26 +0800

    add test

---
+++ test/index.spec.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const assert = require('assert')
+const aTask = require('..')
+
+describe('airy-task', function () {
+    it('normal', function (done) {
+        let arr = [1, 2, 3, 4, 5]
+        let orderList = []
+
+        aTask(arr, ({value}) => {
+            orderList.push(value)
+        }).then(() => {
+            assert.deepEqual(arr, orderList)
+            done()
+        })
+    })
+    it('nest invoke airy-task', function (done) {
+        let arr = [1, 2, 3]
+        let orderList = []
+
+        aTask(arr, ({value}) => {
+            orderList.push(value)
+            if (value === 2) {
+                return aTask(arr, ({value}) => {
+                    orderList.push(value)
+                })
+            }
+        }).then(() => {
+            assert.deepEqual(arr, [1, 2, 3])
+            assert.deepEqual(orderList, [1, 2, 1, 2, 3, 3])
+            done()
+        })
+    })
+    it('support return promise', function (done) {
+        let arr = [1, 2, 3]
+        let orderList = []
+
+        aTask(arr, ({value}) => {
+            if (value === 1) {
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                        orderList.push(100);
+                        resolve()
+                    }, 20)
+                });
+            }
+            orderList.push(value)
+        }).then(() => {
+            assert.deepEqual(arr, [1, 2, 3])
+            assert.deepEqual(orderList, [100, 2, 3])
+            done()
+        })
+    })
+    it('task can be cancel ', function (done) {
+        aTask([1, 2, 3], () => {
+            return false
+        }).then(() => {
+            throw(new Error('not cancelable'))
+        }).catch(() => {
+            done();
+        })
+    })
+})
